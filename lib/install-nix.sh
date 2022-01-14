a9967fdc42f89253e3b1f4c3db70f9776d577aeb
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-14 03:48:16 +0100

    wip

--- lib/install-nix.sh
+++ lib/install-nix.sh
@@ -7,11 +7,7 @@ if ! id "$scriptuser" &>/dev/null
   adduser --system "$scriptuser"
   scriptuser_created=yes
 fi
-sudo -u "$scriptuser" command1
-sudo -u "$scriptuser" command2
-sudo -u "$scriptuser" command3
+sudo -u "$scriptuser" ./install-nix-wrapped.sh
 if [ yes = "$scriptuser_created" ]; then
   userdel "$scriptuser"
 fi
-
-./install-nix-wrapped.sh
