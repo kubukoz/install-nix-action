842659d3222d92fba5b17a53f0fcf7b38f0a048b
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-14 03:49:49 +0100

    spaces?

--- lib/install-nix.sh
+++ lib/install-nix.sh
@@ -3,10 +3,12 @@ set -euo pipefail
 
 scriptuser_created=no
 scriptuser=nix-installer-user
+
 if ! id "$scriptuser" &>/dev/null
   adduser --system "$scriptuser"
   scriptuser_created=yes
 fi
+
 sudo -u "$scriptuser" ./install-nix-wrapped.sh
 if [ yes = "$scriptuser_created" ]; then
   userdel "$scriptuser"
